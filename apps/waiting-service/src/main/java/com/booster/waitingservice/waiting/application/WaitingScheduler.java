45bc836ca76eaea863db129940f4815340699344
ncn-juhongkim <juhongkim@nc-and.com>
2026-01-05 11:53:52 +0900

    etc/ 불필요한 이모티콘 제거

--- apps/waiting-service/src/main/java/com/booster/waitingservice/waiting/application/WaitingScheduler.java
+++ apps/waiting-service/src/main/java/com/booster/waitingservice/waiting/application/WaitingScheduler.java
@@ -27,7 +27,7 @@ public class WaitingScheduler {
     @SchedulerLock(name = "cleanupWaiting", lockAtMostFor = "50s", lockAtLeastFor = "30s")
     @Transactional
     public void cleanup() {
-        log.info("🧹 [Scheduler] 대기열 초기화 작업 시작...");
+        log.info("[Scheduler] 대기열 초기화 작업 시작...");
 
         // 1. DB 정리: 아직도 'WAITING'인 상태인 것들 -> 'CANCELED'로 일괄 변경
         // (JPA 벌크 연산 사용)
@@ -45,13 +45,13 @@ public class WaitingScheduler {
 
     // 1분마다 실행 (cron = "초 분 시 일 월 요일")
     @Scheduled(cron = "0 * * * * *")
-    // 🔒 ShedLock: "checkNoShow"라는 이름으로 잠금을 걺.
+    // ShedLock: "checkNoShow"라는 이름으로 잠금을 걺.
     // lockAtMostFor: 작업이 아무리 길어져도 59초 뒤엔 락을 푼다 (데드락 방지)
     // lockAtLeastFor: 작업이 0.1초 만에 끝나도 최소 10초간은 락을 유지한다 (중복 실행 방지)
     @SchedulerLock(name = "checkNoShow", lockAtMostFor = "59s", lockAtLeastFor = "10s")
     @Transactional
     public void checkNoShow() {
-        log.info("👻 노쇼(No-Show) 처리 스케줄러 시작");
+        log.info("노쇼(No-Show) 처리 스케줄러 시작");
 
         // 기준 시간: 현재로부터 5분 전
         LocalDateTime limitTime = LocalDateTime.now().minusMinutes(5);
@@ -60,7 +60,7 @@ public class WaitingScheduler {
         int updatedCount = waitingRepository.updateStatusToNoShow(limitTime);
 
         if (updatedCount > 0) {
-            log.info("👻 {}명의 대기자가 노쇼로 처리되었습니다.", updatedCount);
+            log.info("{}명의 대기자가 노쇼로 처리되었습니다.", updatedCount);
         }
     }
 }
