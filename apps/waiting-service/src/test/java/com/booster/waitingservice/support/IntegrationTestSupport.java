6395e75828c6006ed42982a099e9ce4ec6e80820
Juhong Kim <wer2wer@naver.com>
2026-01-05 21:18:25 +0900

    [feat] 게이트웨이 세팅 완료

--- apps/waiting-service/src/test/java/com/booster/waitingservice/support/IntegrationTestSupport.java
+++ apps/waiting-service/src/test/java/com/booster/waitingservice/support/IntegrationTestSupport.java
@@ -8,8 +8,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest(properties = {
         "spring.kafka.bootstrap-servers=localhost:9092",
         "spring.kafka.consumer.group-id=test-group"
-//        "spring.jpa.hibernate.ddl-auto=create",  // 종료 시 drop 안 함!
-//        "spring.jpa.open-in-view=false"          // 종료 시 커넥션 바로 반납!
 })
 @Testcontainers(disabledWithoutDocker = true)
 public abstract class IntegrationTestSupport {
