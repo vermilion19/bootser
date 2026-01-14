aa6466f48d335973bff506839310e7411ea04441
Juhong Kim <wer2wer@naver.com>
2026-01-15 08:26:26 +0900

    delete/pro 프로모션 서비스 삭제

--- apps/promotion-service/src/main/java/com/booster/promotionservice/coupon/domain/CouponStatus.java
+++
@@ -1,7 +0,0 @@
-package com.booster.promotionservice.coupon.domain;
-
-public enum CouponStatus {
-    UNUSED,   // 미사용
-    USED,     // 사용 완료
-    EXPIRED   // 만료
-}
--- apps/promotion-service/src/main/java/com/booster/promotionservice/coupon/domain/DiscountType.java
+++
@@ -1,6 +0,0 @@
-package com.booster.promotionservice.coupon.domain;
-
-public enum DiscountType {
-    FIXED,      // 정액 할인 (ex: 5000원 할인)
-    PERCENTAGE  // 정률 할인 (ex: 10% 할인)
-}
