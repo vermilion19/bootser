6c595bc56566268ea6c012e7c41e44fe3cd7803a
Juhong Kim <wer2wer@naver.com>
2026-01-14 13:07:38 +0900

    etc/wait 미사용 의존성 삭제

--- apps/waiting-service/src/test/java/com/booster/waitingservice/waiting/web/controller/WaitingControllerTest.java
+++ apps/waiting-service/src/test/java/com/booster/waitingservice/waiting/web/controller/WaitingControllerTest.java
@@ -17,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
