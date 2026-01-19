09e63edb706c98dd385ef6aeba7086ab739494b9
Juhong Kim <wer2wer@naver.com>
2026-01-20 08:30:32 +0900

    inti/coin api 서비스 init

---
+++ apps/coin/coin-service/src/main/java/com/booster/coinservice/CoinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.booster.coinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinServiceApplication.class, args);
+    }
+
+}
---
+++ apps/coin/coin-service/src/test/java/com/booster/coinservice/CoinServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.booster.coinservice;
+
+import org.junit.jupiter.api.Test;
+
+class CoinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
