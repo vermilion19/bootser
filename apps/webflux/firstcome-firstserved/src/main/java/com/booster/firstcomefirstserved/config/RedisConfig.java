88089bcd6a788bb09c8498d8e7c43e6f6e72a231
Juhong Kim <wer2wer@naver.com>
2026-01-26 14:33:42 +0900

    modify/first 레디스 설정 수정

--- apps/webflux/firstcome-firstserved/src/main/java/com/booster/firstcomefirstserved/config/RedisConfig.java
+++ apps/webflux/firstcome-firstserved/src/main/java/com/booster/firstcomefirstserved/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.booster.firstcomefirstserved.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
         RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
                 .<String, String>newSerializationContext(new StringRedisSerializer())
