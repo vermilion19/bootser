196887e03ac49c90d0bc901dd238435b65d3f7d2
Juhong Kim <wer2wer@naver.com>
2026-02-25 08:54:49 +0900

    chore(kotlin): sample-service, test-service 모듈 삭제
    더 이상 사용하지 않는 두 Kotlin 서비스 모듈을 제거하고
    settings.gradle에서 include 항목도 함께 정리

--- apps/kotlin/sample-service/src/main/kotlin/com/booster/kotlin/sample/domain/SampleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.booster.kotlin.sample.domain
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SampleRepository : JpaRepository<Sample, Long> {
-    fun findByName(name: String): Sample?
-}
--- apps/kotlin/test-service/src/main/kotlin/com/booster/kotlin/testservice/domain/AnimalRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.booster.kotlin.testservice.domain
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AnimalRepository : JpaRepository<Animal, Long> {
-}
