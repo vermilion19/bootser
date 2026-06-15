6e90340386b4dcbea4bc6edb453b2c66bc58a515
Juhong Kim <wer2wer@naver.com>
2026-06-16 08:51:42 +0900

    chore(playground): 0/1 배낭(Knapsack) DP 문제 골격 추가
    알고리즘 학습용 DP 클래스에 0/1 배낭 문제 설명과
    입력 데이터 골격(무게/가치 배열, 용량 W) 작성

---
+++ playground/java-coding-test/src/main/java/play/DP.java
@@ -0,0 +1,21 @@
+package play;
+
+/**
+ * 문제 4. 0/1 배낭 (Knapsack)
+ * n개의 물건에 각각 무게와 가치가 있습니다. 배낭의 최대 용량이 W일 때, 담을 수 있는 가치 합의 최대를 구하세요. 각 물건은 한 번만 사용할 수 있습니다.
+ *
+ * 1 ≤ n ≤ 100
+ * 1 ≤ W ≤ 1,000
+ */
+public class DP {
+
+    static void main() {
+
+        int[] weight = {2, 3, 4, 5};
+        int[] value  = {3, 4, 5, 6};
+        int W = 5;
+        int n = 4;
+
+
+    }
+}
